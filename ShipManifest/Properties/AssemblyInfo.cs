481fc3df8a8cf46e6036e40e8bcb1b227829d0c0
PapaJoesSoup <PapaJoesSoup@users.noreply.github.com>
2014-07-17 23:10:26 -0500

    Update AssemblyInfo.cs

--- ShipManifest/Properties/AssemblyInfo.cs
+++ ShipManifest/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShipManifest")]
-[assembly: AssemblyDescription("Shhip Manifest Manage Crew and Resources")]
+[assembly: AssemblyDescription("Shhip Manifest Manage Resources")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PapaJoe's SOUP")]
 [assembly: AssemblyProduct("ShipManifest")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3.0")]
-[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyVersion("0.3.3.1")]
+[assembly: AssemblyFileVersion("0.3.3.1")]
