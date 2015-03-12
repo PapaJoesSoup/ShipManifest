0eaf198e18c637b23d14226b02b1ddf391884074
PapaJoesSoup <joekorinek@cox.net>
2015-03-12 14:11:00 -0500

    correct versions

--- ShipManifest/Properties/AssemblyInfo.cs
+++ ShipManifest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PapaJoe's SOUP")]
 [assembly: AssemblyProduct("ShipManifest")]
-[assembly: AssemblyCopyright("Copyright ©  2013, 2014")]
+[assembly: AssemblyCopyright("Copyright ©  2013, 2014, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.1.3")]
-[assembly: AssemblyFileVersion("0.4.1.3")]
+[assembly: AssemblyVersion("4.1.3.0")]
+[assembly: AssemblyFileVersion("4.1.3.0")]
