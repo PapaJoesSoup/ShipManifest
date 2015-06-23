30e3bd126900fda9cdc952a6864c5cb759b15780
PapaJoesSoup <joekorinek@cox.net>
2015-06-23 09:36:17 -0500

    Update change log and remove debug messages

--- ShipManifest/WindowRoster.cs
+++ ShipManifest/WindowRoster.cs
@@ -333,7 +333,6 @@ namespace ShipManifest
         {
             if (DFInterface.IsDFInstalled)
             {
-                Utilities.LogMessage("GetFrozenKerbals.  Loading", "Info", true);
                 return DFInterface.GetFrozenKerbals().FrozenKerbals;
             }
             else
